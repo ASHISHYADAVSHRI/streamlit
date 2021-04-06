f009a9d41cd096283b608d99a17c655e88b05fda
Shri Yadav <78556556+ASHISHYADAVSHRI@users.noreply.github.com>
2021-04-06 20:55:09 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import streamlit as sl
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error , r2_score
-
+import scikit_learn
 sl.markdown(
 	"""
 	<style>
